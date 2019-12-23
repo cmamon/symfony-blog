b01a3032c843f72194fe11bc928d23ae9bfba083
SullyvanT <tremor.sullyvan@gmail.com>
2019-12-23 02:51:31 +0100

    Enable a post at least a image or text

--- src/Form/Type/PostFormType.php
+++ src/Form/Type/PostFormType.php
@@ -13,14 +13,20 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class PostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'required' => false
+            ])
             ->add('content', CKEditorType::class, [
+              'constraints' => new Callback(array($this, 'validate')),
+              'required' => false,
                 'attr' => ['class' => 'ckeditor'],
             ])
             ->add('submit', SubmitType::class)
@@ -37,5 +43,17 @@ class PostFormType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Post::class,
         ]);
+    }
+
+    public function validate($value, ExecutionContextInterface $context)
+    {
+        $form = $context->getRoot();
+        $data = $form->getData();
+
+        if (empty($data->getContent()) && empty($data->getImage())) {
+            $context->buildViolation('This post need at least a description or image')
+            ->atPath('content')
+            ->addViolation();
+        }
     }
 }
